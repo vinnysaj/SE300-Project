fd664b6761be7ccc348ee068f44e8460d41f03f5
Vincent Sajkowski <vinnysaj@icloud.com>
2024-02-28 20:09:58 -0500

    Disable anchor element entirely on homepage by deleting it to avoid pointer events

--- assets/react/controllers/Navbar/Navbar.tsx
+++ assets/react/controllers/Navbar/Navbar.tsx
@@ -1,23 +1,26 @@
-import React, {ReactElement, useState} from 'react';
+import React, {ReactElement, useLayoutEffect, useState} from 'react';
 
 export default function (properties: NavbarProps):ReactElement {
-    const [redirect, setRedirect] = useState("");
-    const getRedirect = () => {
-        console.log("Here");
-        if(window.location.pathname == "/home") {
-            return("#");
+    const [homeEnabled, setHomeEnabled] = React.useState(true);
+
+    useLayoutEffect(() => {
+        if (window.location.pathname === "/" || window.location.pathname === "/home") {
+            setHomeEnabled(false);
         }
-        return("/home")
-    }
+    }, []);
+
     return (
         <div className="w-full h-12 rounded-b-lg fixed bg-black backdrop-blur-xl opacity-80 z-50">
             <div className="flex items-center justify-between h-full">
                 <div className="ml-4">
                     <div className="text-xl text-gray-200">
-                        <a
-                            href={getRedirect()}>
+                        {homeEnabled ? (
+                            <a href="/" className={homeEnabled ? "text-white" : "disabled"}>
+                                <span className="font-bold text-white">BoundlessFlight</span>
+                            </a>
+                        ): (
                             <span className="font-bold text-white">BoundlessFlight</span>
-                        </a>
+                        )}
                         <span className="mx-2">//</span>
                         <span>{properties.pageTitle}</span>
                     </div>
