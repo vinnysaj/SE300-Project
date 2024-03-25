e2b819bc885da0dd8c86ff8e4817d1af3e2fb9b6
Vincent Sajkowski <vinnysaj@icloud.com>
2024-03-25 13:55:47 -0400

    Added ability to pull plane information from the backend database

--- assets/react/controllers/Dashboard/Dashboard.tsx
+++ assets/react/controllers/Dashboard/Dashboard.tsx
@@ -4,15 +4,33 @@ import DashboardGrid from "../Dashboard/DashboardGrid";
 import DashboardGridElement from "../Dashboard/DashboardGridElement";
 import NavbarHelper from "../Navbar/NavbarHelper";
 import DashboardGridAddElement from "../Dashboard/DashboardGridAddElement";
+import {makeAuthCall} from "../AuthManager/AuthManager";
+import {interactive} from "@material-tailwind/react/types/components/popover";
+import PlaneDetails from "../PlaneDetails/PlaneDetails";
 
 const Dashboard: React.FC = () => {
     const [planeGridElements, setPlaneGridElements] = useState(null)
     React.useEffect(() => {
-        const gridElements = [
-            <DashboardGridElement key={0} planeImgPath={"/images/test.jpg"} planeName={"My Plane"} planeID={"testID"}></DashboardGridElement>,
-            <DashboardGridAddElement key={1} addText="Add new" />
-        ]
-        setPlaneGridElements(gridElements)
+        const getPlanesURL = "https://api.boundlessflight.net/api/user/get/assignedaircraft"
+        let response = makeAuthCall(getPlanesURL, "GET", null);
+        response.then((res) => {
+            let planeDataArray: PlaneData[] = res.data as PlaneData[];
+            const gridElements:React.JSX.Element[] = [];
+            for (let i = 0; i < planeDataArray.length; i++) {
+                gridElements.push(<DashboardGridElement key={i} planeImgPath={"/images/test.jpg"} planeName={planeDataArray[i].reg} planeID={planeDataArray[i].id.toString()}></DashboardGridElement>)
+            }
+            gridElements.push(<DashboardGridAddElement key={planeDataArray.length} addText="Add new" />)
+            setPlaneGridElements(gridElements);
+        }).catch((err) => {
+            console.log(err);
+            // alert with options to redirect or close
+            let result = confirm("Error fetching planes! Perhaps you are not authenticated? Press 'OK' to sign in.");
+            if (result) {
+                window.location.href = "https://auth.boundlessflight.net";
+            } else {
+                alert("so be it. >:(")
+            }
+        });
     }, []);
 
     return (
@@ -25,5 +43,23 @@ const Dashboard: React.FC = () => {
         </div>
     );
 };
+
+interface PlaneData {
+    id: number;
+    reg: string;
+    active: boolean;
+    serial: string;
+    icao: string;
+    model: string;
+    typeName: string;
+    regowner: string;
+    hours: number;
+    plane_data: any;
+    owner_id: any;
+    createdAt: string;
+    updatedAt: string;
+    icon_file_b64: string;
+    cover_file_b64: string;
+}
 
 export default Dashboard;
